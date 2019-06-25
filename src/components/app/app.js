f4d41146749cc8f6e8b4ea79624eb69903df9bb3
Елена <malinovael@yandex.ru>
2019-06-25 14:05:17 +0300

    header is prepared

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from '../header';
+//import BurgerMenu from '../burger-menu';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <Header/>
+            </div>
+        );
+    };
+};
+//<BurgerMenu
+//open = {this.toggleBurgerMenu} />
--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,35 +1,85 @@
 import React from 'react';
+import BurgerMenu from '../burger-menu';
 import './header.css';
 
-export default class Header extends React.Component {
+class Header extends React.Component {
+    state = {
+        openlogin: false,
+        openLang: false,
+        openCurrency: false
+    };
+    onMenuLogin = () => {
+        this.setState((state) => {
+            return {
+                openlogin: !state.openlogin
+            };
+        })
+    };
+    onMenuItemCurrency = () => {
+        this.setState((state) => {
+            return {
+                openCurrency: !state.openCurrency
+            };
+        });
+    }
+    onMenuItemLang = () => {
+        this.setState((state) => {
+            return {
+                openLang: !state.openLang,
+            };
+        });
+    }
     render() {
+        const { openlogin , openLang , openCurrency } = this.state;
+        let classNames = {
+            logIn:"log-in__menu",
+            currency:"currency__menu",
+            lang:"lang__menu",
+        }
+
+        if (openlogin) {
+            classNames.logIn += " open";
+        } else if (openCurrency) {
+            classNames.currency += " open";
+        } else if (openLang) {
+            classNames.lang += "  open";
+        }
         return (
-           <header>
-               <nav className="main-nav header-top">
-                   <ul className="burger-menu">
-                   <li>Популярные рейсы</li>
-                   <li>Расписание рейсов</li>
-                   <li>Забронированные рейсы</li>
-                   <li>Вход для партнеров</li>
-                   <li>О компании</li>
-                   <li>Контакты</li>
+           <header className="header">
+               <nav className="header-top main-nav">
+                   <BurgerMenu/>
+                    <ul className="menu">
+                        <li className="menu-item log-in" onMouseOver = { this.onMenuLogin } onMouseOut = { this.onMenuLogin }>Личный кабинет
+                            <ul className={classNames.logIn} >
+                                <li>Вход</li>
+                                <li>Регистрация</li>
+                            </ul>
+                        </li>
+                        <li className="menu-item currency" onMouseOver = { this.onMenuItemCurrency } onMouseOut = { this.onMenuItemCurrency }>Рубль
+                            <ul className={classNames.currency}>
+                                <li>Euro</li>
+                                <li>Dollar</li>
+                            </ul>
+                        </li>
+                        <li className="menu-item lang" onMouseOver = { this.onMenuItemLang } onMouseOut = { this.onMenuItemLang } >Ru
+                            <ul className={classNames.lang}>
+                                <li>En</li>
+                            </ul>
+                        </li>
+                        <li className="menu-item telephone">8-800-500-61-01<br/><b>Круглосуточная поддержка</b></li>
                     </ul>
-               <ul className="menu">
-                   <li>Личный кабинет</li>
-                   <li>Рубль</li>
-                   <li>RU</li>
-                   <li>8-800-500-61-01</li>
-               </ul>
                </nav>
                <div className="header-middle">
-                   <a className="header__logo" href="#">
-                       <img src="" alt=""/>
+                   <a className="header__logo" href="https://yandex.ru">
+                      <img src="https://s4.aconvert.com/convert/p3r68-cdx67/cb04a-0b7eq.svg" alt=""/>
                    </a>
                    <h3>Морские прогулки</h3>
                    <h2>по черному морю</h2>
-                   <a href="#" className="header-middle__button">Выбрать круиз</a>
+                   <a href="https://yandex.ru" className="header-middle__button">Выбрать круиз</a>
                </div>
            </header>
         )
     }
 };
+export default Header;
+///подумать над классами!!! как лучше!!!! 
