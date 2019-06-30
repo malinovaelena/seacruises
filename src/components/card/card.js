7c600382a1115b5da137f169a16c50379be9e1e7
Елена <malinovael@yandex.ru>
2019-06-30 13:35:48 +0300

    fix state main

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -7,22 +7,16 @@ export default class Card extends React.Component {
         this.arrOfCards = data;
     }
     render() {
-        const amountOfCards = 5;
-        const  newarr = this.arrOfCards.slice(0, amountOfCards);
-        return (
-        newarr.map((item) => {
-        console.log(item);
         return (<article className="card-item">
         <div className="cards-image__container">
             <img className="cards-image" src={require('./img/test.png')} alt="anapa"/>
         </div>
         <section className="card-item__description">
-            <h4 className="description-name">{ item.name }</h4>
-            <p className="track-shedule"><b>Маршрут:</b>{ item.tracking }</p>
-            <p className="duration"><b>Продолжительность:</b>{item.hour} ч. {item.minutes} мин.</p>
+            <h4 className="description-name">{ this.arrOfCards.name }</h4>
+            <p className="track-shedule"><b>Маршрут:</b>{ this.arrOfCards.tracking }</p>
+            <p className="duration"><b>Продолжительность:</b>{this.arrOfCards.hour} ч. {this.arrOfCards.minutes} мин.</p>
             <p className="price">27 000 руб</p>
         </section>
     </article>)
-        })
-    )}
+    }
 };
--- src/components/main/main.js
+++ src/components/main/main.js
@@ -3,22 +3,61 @@ import Card from '../card';
 import ArrDataOfCards from '../generate-data';
 import './main.css';
 
+
 export default class Main extends React.Component {
     constructor() {
         super();
-        this.renderAmount = (amountForRender) => {
-            for (let i = 0; i < amountForRender; i++) {
-                return (<Card data = { ArrDataOfCards[i]}/>);
-            };
-        };
+        this.superddata = ArrDataOfCards;
     }
+    readynewCards = (arr,amountOfCards) => {
+        const  newarr = arr.slice(0, amountOfCards);
+        return (
+            newarr.map((item) => {
+                console.log(item);
+                return <Card data = {item}/>
+            })
+        );
+    };
+    showMoreCards = () => {
+        //this.readynewCards(this.superddata.slice(0,7));
+        let rrr = this.superddata.slice(0,7);
+        return (
+            rrr.map((item) => {
+                console.log(item,'showMoreCards');
+                return <Card data = {item}/>
+            })
+        )
+    };
+    /*
+    const showMoreCards = (cards) => {
+    console.log('showMoreCards');
+    renderFilms(cards.slice(0, cardToRenderPosition));
+    if (cardToRenderPosition >= cards.length) {
+    showMoreButton.removeEventListener(`click`, onShowMoreButtonClick);
+    showMoreButton.classList.add(`visually-hidden`);
+    }
+    cardToRenderPosition += AMOUNT_FOR_RENDER_CARDS;
+    };
+    */
+
     render() {
+        let zero = 1;
+        let item = () => {
+            zero = zero + 5;
+            console.log(zero,'ZERO');
+        };
+        let we = () => {
+            console.log(zero,'ZERO');
+        }
+            //с помощью клика увечичивать переменную которая будет ответстна за массив1!!!
+        //переписать функцию по поводу раскладки массива. В контейнере должно быть - визибл айтемс
+        //заебись все работает1!!!В ВОСКРЕСЕНЬЕ СДЕЛАТЬ ЭТО (ПОКАЗ НОВЫХ КАРТОЧЕК) ПЛЮС ФУТЕР !!!
         return (<div className="main">
                 <h2 className="main__title">Каталог круизов</h2>
                 <h3 className="main__subtitle">Сортировать круизы по <b className="main__filter">цене</b></h3>
-                <div className="cards-container">{<Card data = {ArrDataOfCards}/>}
+                <div className="cards-container" onClick={we}>{this.readynewCards(ArrDataOfCards,4)}
                 </div>
-                <button className="cards-list__show-more">Показать больше</button>
+                <button className="cards-list__show-more" onClick={item}>Показать больше</button>
                 </div>
         )
     }
